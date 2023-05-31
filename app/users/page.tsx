dd0a155d8d00c5c11e7e1747997e82dac083ec68
Christian Salvadora <christianrsalvadora@gmail.com>
2023-06-01 01:07:31 +0800

    Installed prettier

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,27 +1,25 @@
 async function fetchUsers(pageIndex: number = 1) {
-    const response = await fetch(`https://reqres.in/api/users?page=${pageIndex}`);
-    return response.json();
+  const response = await fetch(`https://reqres.in/api/users?page=${pageIndex}`)
+  return response.json()
 }
 
 interface User {
-    id: number;
-    email: string
-    first_name: string
-    last_name: string
-    avatar: string
+  id: number
+  email: string
+  first_name: string
+  last_name: string
+  avatar: string
 }
 
 export default async function UsersPage() {
-    let data = await fetchUsers();
-    let users = data.data;
-    
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            {users.map((user : User) => {
-                return (
-                <div>{user.first_name}</div>
-                )
-            })}
-        </div>
-    )
+  let data = await fetchUsers()
+  let users = data.data
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      {users.map((user: User) => {
+        return <div>{user.first_name}</div>
+      })}
+    </div>
+  )
 }
