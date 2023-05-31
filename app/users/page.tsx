8231cf93a0046ec802289e098ebc9b7d2e8adf6c
Christian Salvadora <christianrsalvadora@gmail.com>
2023-06-01 02:00:55 +0800

    Render users

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,14 +1,11 @@
+'use client'
+
+import UserCard from '@/components/user-card'
+import { User } from '@/interfaces/users'
+
 async function fetchUsers(pageIndex: number = 1) {
   const response = await fetch(`https://reqres.in/api/users?page=${pageIndex}`)
   return response.json()
-}
-
-interface User {
-  id: number
-  email: string
-  first_name: string
-  last_name: string
-  avatar: string
 }
 
 export default async function UsersPage() {
@@ -16,10 +13,18 @@ export default async function UsersPage() {
   let users = data.data
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      {users.map((user: User) => {
-        return <div>{user.first_name}</div>
-      })}
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-white">
+      <div className="grid grid-cols-3 gap-8 m-8">
+        {users.map((user: User) => {
+          return <UserCard key={user.id} user={user} />
+        })}
+      </div>
+      <button
+        className="bg-green-600 p-4"
+        onClick={() => console.log('clicked')}
+      >
+        Load more
+      </button>
     </div>
   )
 }
